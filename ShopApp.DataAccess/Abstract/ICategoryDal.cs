b07b16feb8bb03fd37f2f735fd1ab7e41b951f45
Tanju BOZOK <tnjbzk@gmail.com>
2021-12-21 03:56:48 +0400

    DataAccess katmanı için Repository, GenericSınıflar ve entityler eklemesi yapıldı.

---
+++ ShopApp.DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,13 @@
+﻿using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+using System.Text;
+
+namespace ShopApp.DataAccess.Abstract
+{
+    public interface ICategoryDal : IRepositoryDal<Category>
+    {
+    }
+}

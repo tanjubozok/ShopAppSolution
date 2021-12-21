f3522e616008e760739ef55869fedad9aa040039
Tanju BOZOK <tnjbzk@gmail.com>
2021-12-21 04:07:22 +0400

    Business katmanı için sınıflar oluşturuldu.

---
+++ ShopApp.Business/Abstract/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.Business.Abstract
+{
+    public interface ICategoryService
+    {
+        List<Category> GetAll();
+        Category GetById(int id);
+
+        void Create(Category entity);
+        void Update(Category entity);
+        void Delete(Category entity);
+    }
+}
---
+++ ShopApp.Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+
+        void Create(Product entity);
+        void Update(Product entity);
+        void Delete(Product entity);
+    }
+}

1345af1e24b935555aef2b899e6fdfc1ba8d75b6
Tanju BOZOK <tanjubozok@hotmail.com>
2021-12-28 02:27:20 +0400

    commiyt..

--- ShopApp.WebUI/Controllers/AdminController.cs
+++ ShopApp.WebUI/Controllers/AdminController.cs
@@ -1,12 +1,96 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using ShopApp.Business.Abstract;
+using ShopApp.Entities;
+using ShopApp.WebUI.Models;
 
 namespace ShopApp.WebUI.Controllers
 {
     public class AdminController : Controller
     {
+        private IProductService _productService;
+
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Index()
         {
+            var getProductList = _productService.GetAll();
+            var model = new ProductListModel
+            {
+                Products = getProductList
+            };
+            return View(model);
+        }
+
+        public IActionResult CreateProduct()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductModel model)
+        {
+            var entity = new Product
+            {
+                Description = model.Description,
+                ImageUrl = model.ImageUrl,
+                Name = model.Name,
+                Price = model.Price
+            };
+            _productService.Create(entity);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var entity = _productService.GetById((int)id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            var model = new ProductModel()
+            {
+                Description = entity.Description,
+                Id = entity.Id,
+                ImageUrl = entity.ImageUrl,
+                Name = entity.Name,
+                Price = entity.Price
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ProductModel model)
+        {
+            var entity = _productService.GetById(model.Id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            entity.Description = model.Description;
+            entity.ImageUrl = model.ImageUrl;
+            entity.Name = model.Name;
+            entity.Price = model.Price;
+            _productService.Update(entity);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int productId)
+        {
+            var entity = _productService.GetById(productId);
+            if (entity != null)
+            {
+                _productService.Delete(entity);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
